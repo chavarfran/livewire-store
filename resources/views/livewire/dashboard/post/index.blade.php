59223884eda3e9aa24d6620d72331864a0553987
Francisco Chavarria <fchavarriar@outlook.com>
2022-11-22 08:42:51 -0600

    module post

---
+++ resources/views/livewire/dashboard/post/index.blade.php
@@ -0,0 +1,153 @@
+@slot('header')
+    {{ __('Posts') }}
+@endslot
+<x-card class="container">
+
+    <x-jet-action-message on="deleted">
+        <div class="box-action-message">
+            {{ __('Post deleted success') }}
+        </div>
+    </x-jet-action-message>
+
+    @slot('title')
+        Listado de posts
+    @endslot
+
+    <div class="grid grid-cols-2 mb-2 gap-2">
+        <x-jet-input class="w-full" type="text" wire:model.debounce.500ms="search"
+            placeholder="Buscar por id, tìtulo o descripción"></x-jet-input>
+        <div class="grid grid-cols-2 gap-2">
+            <x-jet-input class="w-full" type="date" wire:model="from" placeholder="Desde"></x-jet-input>
+            <x-jet-input class="w-full" type="date" wire:model="to" placeholder="Hasta"></x-jet-input>
+        </div>
+    </div>
+
+    <div class="flex gap-2 mb-2">
+
+        <select class="block w-full" wire:model="posted">
+            <option value="">{{ __('Posted') }}</option>
+            <option value="not">No</option>
+            <option value="yes">Si</option>
+        </select>
+
+        <select class="block w-full" wire:model="type">
+            <option value="">{{ __('Type') }}</option>
+            <option value="adverd">adverd</option>
+            <option value="post">post</option>
+            <option value="course">course</option>
+            <option value="movie">movie</option>
+        </select>
+
+        <select class="block w-full" wire:model="category_id">
+            <option value="">{{ __('Category') }}</option>
+            @foreach ($categories as $i => $c)
+                <option value="{{ $i }}">{{ $c }}</option>
+            @endforeach
+        </select>
+
+    </div>
+
+    <table class="table w-full border">
+        <thead class="text-left bg-gray-100 ">
+            <tr class="border-b">
+                @foreach ($columns as $key => $c)
+                    <th>
+                        <button wire:click="sort('{{ $key }}')">
+                            {{ $c }}
+                            @if ($key == $sortColumn)
+                                @if ($sortDirection == 'asc')
+                                    &uarr;
+                                @else
+                                    &darr;
+                                @endif
+                            @endif
+                            
+                        </button>
+                    </th>
+                @endforeach
+                <th class="p-2">
+                    Acciones
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $p)
+                <tr class="border-b">
+                    <td class="p-2">
+                        {{ $p->id }}
+                    </td>
+                    <td class="p-2">
+                        {{ str($p->title)->substr(0, 15) }}
+                    </td>
+                    <td class="p-2">
+                        {{ $p->date }}
+                    </td>
+                    <td class="p-2">
+                        <textarea class="w-48">
+                        {{ $p->description }}
+                       </textarea>
+                    </td>
+                    <td class="p-2">
+                        {{ $p->posted }}
+                    </td>
+                    <td class="p-2">
+                        {{ $p->type }}
+                    </td>
+                    <td class="p-2">
+                        {{ $p->category->title }}
+                    </td>
+                    <td class="p-2">
+                        <x-jet-nav-link href="{{ route('d-post-edit', $p) }}" class="mr-2">EDITAR
+                        </x-jet-nav-link>
+                        <x-jet-danger-button {{-- onclick="confirm('Seguro que deseas eliminar el registro seleccionado?') || event.stopImmediatePropagation()" --}}
+                            wire:click="seletedPostToDelete({{ $p }})">
+                            Eliminar
+                        </x-jet-danger-button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <br>
+
+    <a class="btn-secondary mb-3" href="{{ route('d-post-create') }}">Crear post</a>
+    {{ $posts->links() }}
+
+    <x-jet-confirmation-modal wire:model="confirmingDeletePost">
+        <x-slot name="title">
+            {{ __('ELiminar Post') }}
+        </x-slot>
+
+        <x-slot name="content">
+            {{ __('Esta seguro de eliminar este post?') }}
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('confirmingDeletePost')" wire:loading.attr="disabled">
+                {{ __('Cancelar') }}
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button class="ml-3" wire:click="delete()" wire:loading.attr="disabled">
+                {{ __('Eliminar') }}
+            </x-jet-danger-button>
+        </x-slot>
+    </x-jet-confirmation-modal>
+</x-card>
+
+<script>
+    window.onload = function(){
+        Livewire.hook("element.updated",(el,component)=>{
+           // console.log(component)
+        })
+        Livewire.hook("element.updating",(fromEl,toEl,component)=>{
+            console.log(component)
+        })
+        Livewire.hook("element.removed",(el,component)=>{
+            //console.log(el)
+        })
+        Livewire.hook("message.sent",(message,component)=>{
+         //   console.log(message)
+          //  console.log(message.updateQueue[0].payload.method)
+        })
+    }
+</script>
---
+++ resources/views/livewire/dashboard/post/save.blade.php
@@ -0,0 +1,140 @@
+@slot('header')
+    {{ __('posts') }}
+@endslot
+
+<div class="container">
+
+    <x-jet-action-message on="created">
+        <div class="box-action-message">
+            {{ __('Created post success') }}
+        </div>
+    </x-jet-action-message>
+
+    <x-jet-action-message on="updated">
+        <div class="box-action-message">
+            {{ __('Updated post success') }}
+        </div>
+    </x-jet-action-message>
+
+    <x-jet-form-section submit="submit">
+
+        @slot('title')
+            {{ __('Posts') }}
+        @endslot
+
+        @slot('description')
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate eaque, iure modi ipsa consequatur, veritatis
+            debitis accusantium quasi consequuntur velit earum nam nisi dolores temporibus animi placeat, alias cumque
+            assumenda.
+        @endslot
+
+        @slot('form')
+
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label for="">Título</x-jet-label>
+                <x-jet-input-error for="title" />
+                <x-jet-input class="block w-full" type="text" wire:model="title" />
+            </div>
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label for="">Fecha</x-jet-label>
+                <x-jet-input-error for="date" />
+                <x-jet-input class="block w-full" type="date" wire:model="date" />
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+                <div wire:ignore>
+                    <div id="ckcontent">
+                        {!! $text !!}
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label for="">Contenido</x-jet-label>
+                <x-jet-input-error for="text" />
+
+                <textarea class="block w-full" wire:model="text"></textarea>
+            </div>
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label for="">Descripción</x-jet-label>
+                <x-jet-input-error for="description" />
+
+                <textarea class="block w-full" wire:model="description"></textarea>
+            </div>
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label for="">Posted</x-jet-label>
+                <x-jet-input-error for="posted" />
+
+                <select class="block w-full" wire:model="posted">
+                    <option value=""></option>
+                    <option value="not">No</option>
+                    <option value="yes">Si</option>
+                </select>
+            </div>
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label for="">Típo</x-jet-label>
+                <x-jet-input-error for="type" />
+
+                <select class="block w-full" wire:model="type">
+                    <option value=""></option>
+                    <option value="adverd">adverd</option>
+                    <option value="post">post</option>
+                    <option value="course">course</option>
+                    <option value="movie">movie</option>
+                </select>
+            </div>
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label for="">Categorías</x-jet-label>
+                <x-jet-input-error for="category_id" />
+
+                <select class="block w-full" wire:model="category_id">
+                    <option value=""></option>
+                    @foreach ($categories as $c)
+                        <option value="{{ $c->id }}">{{ $c->title }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label for="">Imagen</x-jet-label>
+                <x-jet-input-error for="image" />
+                <x-jet-input type="file" wire:model="image" />
+
+                @if ($post && $post->image)
+                    <img class="w-40 my-3" src="{{ $post->getImageUrl() }}" />
+                @endif
+            </div>
+
+        @endslot
+
+        @slot('actions')
+            <x-jet-button type="submit">Enviar</x-jet-button>
+        @endslot
+
+    </x-jet-form-section>
+</div>
+
+<script src="{{ asset('js/ckeditor/ckeditor.js') }}"></script>
+
+<script>
+    document.addEventListener('livewire:load', function() {
+        //@this.text = "texto"
+        // *** comunicacion CKEDITOR a propiedad
+        var ckeditor = null
+        var editor = ClassicEditor.create(document.querySelector("#ckcontent")).then(
+            editor => {
+                ckeditor = editor
+                editor.model.document.on('change:data', () => {
+                    @this.text = editor.getData()
+                });
+            }
+        )
+        // *** comunicacion propiedad a CKEDITOR
+        Livewire.hook('message.processed', (message, component) => {
+
+            if (message.updateQueue[0].name == "text")
+                ckeditor.setData(@this.text)
+        })
+    })
+</script>
